86d39d349c0b638e3edf2ed6053c645a5dd9fd35
mervyn-teo <teomervyn@yahoo.com>
2025-03-11 22:09:49 +0800

    Revert "Add MedicalReport class"

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -24,19 +24,17 @@ public class Person {
     // Data fields
     private final Address address;
     private final Set<Tag> tags = new HashSet<>();
-    private MedicalReport medicalReport;
 
     /**
      * Every field must be present and not null.
      */
-    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags, MedicalReport medicalReport) {
+    public Person(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
         requireAllNonNull(name, phone, email, address, tags);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.tags.addAll(tags);
-        this.medicalReport = medicalReport;
     }
 
     public Name getName() {
