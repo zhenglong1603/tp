c5d554a2ddc737f6e12f489bab5dab5ed008e7a4
mervyn-teo <teomervyn@yahoo.com>
2025-03-11 17:37:21 +0800

    Fix style issues

--- src/main/java/seedu/address/model/doctor/Doctor.java
+++ src/main/java/seedu/address/model/doctor/Doctor.java
@@ -1,30 +1,33 @@
 package seedu.address.model.doctor;
 
-    /**
-     * Represents a Doctor in the address book.
-     * Guarantees: details are present and not null, field values are validated, immutable.
-     */
+/**
+ * Represents a Doctor in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
 
 public class Doctor {
-    private final String Name;
-    private final String NRIC;
-    private final String Specialisation;
+    private final String name;
+    private final String nric;
+    private final String specialisation;
 
-    public Doctor(String Name, String NRIC, String Specialisation) {
-        this.Name = Name;
-        this.NRIC = NRIC;
-        this.Specialisation = Specialisation;
+    /**
+     * Every field must be present and not null.
+     */
+    public Doctor(String name, String nric, String specialisation) {
+        this.name = name;
+        this.nric = nric;
+        this.specialisation = specialisation;
     }
 
     public String getName() {
-        return Name;
+        return name;
     }
 
-    public String getNRIC() {
-        return NRIC;
+    public String getNric() {
+        return nric;
     }
 
     public String getSpecialisation() {
-        return Specialisation;
+        return specialisation;
     }
 }
