aa63f9fd2806c2bc08c8cc5f50615629ba3d3b58
Lim Shao Qi <>
2025-03-15 13:41:09 +0800

    Update SampleDataUtil.java to include MedicalReport

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -6,13 +6,7 @@ import java.util.stream.Collectors;
 
 import seedu.address.model.Klinix;
 import seedu.address.model.ReadOnlyKlinix;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.BirthDate;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Nric;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -25,32 +19,38 @@ public class SampleDataUtil {
                 new Nric("S1234567A"),
                 new BirthDate("1999-01-01"),
                 new Address("Blk 30 Geylang Street 29, #06-40"),
-                getTagSet("friends")),
+                getTagSet("friends"),
+                new MedicalReport("Pollen", "Asthma", "Appendectomy", "COVID-19")),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new Nric("S1234568B"),
                 new BirthDate("1999-01-02"),
                 new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                getTagSet("colleagues", "friends")),
+                getTagSet("colleagues", "friends"),
+                new MedicalReport("None", "Diabetes", "None", "Hepatitis B")),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
                 new Nric("S1234569C"),
                 new BirthDate("1999-01-03"),
                 new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                getTagSet("neighbours")),
+                getTagSet("neighbours"),
+                new MedicalReport("Dust", "None", "Tonsillectomy", "Flu")),
             new Person(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
                 new Nric("S1234569D"),
                 new BirthDate("1999-01-04"),
                 new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                getTagSet("family")),
+                getTagSet("family"),
+                new MedicalReport("Peanuts", "Hypertension", "None", "None")),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
                 new Nric("S1234569E"),
                 new BirthDate("1999-01-05"),
                 new Address("Blk 47 Tampines Street 20, #17-35"),
-                getTagSet("classmates")),
+                getTagSet("classmates"),
+                new MedicalReport("None", "None", "None", "None")),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
                 new Nric("S1234569F"),
                 new BirthDate("1999-01-06"),
                 new Address("Blk 45 Aljunied Street 85, #11-31"),
-                getTagSet("colleagues"))
+                getTagSet("colleagues"),
+                new MedicalReport("Shellfish", "Gout", "Gallbladder Removal", "None"))
         };
     }
 
